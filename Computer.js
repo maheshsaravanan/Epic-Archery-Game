9dd8e74f5aa797efc7d18d330e5d0ebea50fcda2
maheshsaravanan <71903967+maheshsaravanan@users.noreply.github.com>
2021-07-12 21:40:05 -0400

    Add files via upload

---
+++ Computer.js
@@ -0,0 +1,18 @@
+class Computer {
+    constructor(x, y, width, height) {
+        var options = {
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        World.add(world, this.body);
+        this.height = height
+        this.width = width
+        this.image = loadImage("assets/player.png")    
+    }
+    display() {
+        var pos = this.body.position
+        imageMode(CENTER)
+        image(this.image, pos.x, pos.y, this.width, this.height);
+
+    }
+}
